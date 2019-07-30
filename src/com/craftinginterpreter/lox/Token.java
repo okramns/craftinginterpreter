4d86657be09c3c7b9be1567ed2f2ed7df960f6e5
Singh <Okram_Singh@McAfee.com>
2019-07-30 15:20:59 +0530

    Scanner

--- src/com/craftinginterpreter/lox/Token.java
+++ src/com/craftinginterpreter/lox/Token.java
@@ -1,7 +1,21 @@
 package com.craftinginterpreter.lox;
 
-/**
- * Created by OSingh4 on 7/30/2019.
- */
+import com.craftinginterpreter.lox.TokenType;
+
 public class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    Token(TokenType type, String lexeme, Object literal, int line) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
 }
